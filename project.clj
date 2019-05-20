b21e4f0d108fd96c59c7ce6be57d68318c22c2d6
lenz <lenz@mac>
2019-05-20 09:58:42 +0200

    Added clj-kondo

--- project.clj
+++ project.clj
@@ -3,14 +3,23 @@
   :url "https://github.com/l3nz/say-cheez"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]]
+
+  :aliases
+  {"fix" ["cljfmt" "fix"]
+   "clj-kondo" ["trampoline" "run" "-m" "clj-kondo.main" "--" "--lint" "src/" "--cache" ".cli-kondo-cache"]}
+
+  ; deps
+  :dependencies
+  [[org.clojure/clojure "1.9.0" :scope "provided"]
+   [clj-kondo "2019.05.19-alpha" :scope "provided"]]
+
   :main say-cheez.run
   :scm {:name "git"
         ;; :tag "..."
-        :url "https://github.com/l3nz/say-cheez" }
+        :url "https://github.com/l3nz/say-cheez"}
   :plugins [[lein-eftest "0.5.1"]
             [jonase/eastwood "0.2.5"]
             [lein-kibit "0.1.6"]
-            [lein-cljfmt "0.5.7"]]
+            [lein-cljfmt "0.5.7"]]; repos          
   :deploy-repositories [["clojars"  {:sign-releases false :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]])
